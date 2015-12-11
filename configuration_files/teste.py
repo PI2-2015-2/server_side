f702b13f06de8bbff435dd85f98f12257f234d03
levinomoises <levino_moises@hotmail.com>
2015-12-11 10:32:17 -0200

    Test file for the command execution

--- configuration_files/teste.py
+++ configuration_files/teste.py
@@ -4,7 +4,6 @@ from collections import OrderedDict
 
 # Loads the Json file in a OrderedDict
 parsed_json = json.load(open('instruction.json'), object_pairs_hook=OrderedDict)
-#print json.dumps(parsed_json, indent=2)
 
 # Instantiate class runner for instructions
 instruction = ExecInstructions()
@@ -15,12 +14,10 @@ while i < size:
     # Execute inside loop instructions
     if(parsed_json.keys()[i] == 'loop'):
         for k in range(0, parsed_json.values()[i].get('loops')):
-            for j in range(i, i+parsed_json.values()[i].get('instructions')):
+            for j in range(i+1, i+1+parsed_json.values()[i].get('instructions')):
                 instruction.run(parsed_json.keys()[j], parsed_json.values()[j])
-
         # Jump to the next instruction outside the loop
-        i = i+len(parsed_json.values()[i])
-
+        i += 1+parsed_json.values()[i].get('instructions')
     else:
         # execute instruction
         instruction.run(parsed_json.keys()[i], parsed_json.values()[i])
